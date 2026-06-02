c374ce85288582ea48f165e6390013a8bbb280fd
Sabbir <imamsabbir20173@gmail.com>
2026-06-02 20:14:12 +0600

    Add cloudinary to settings and requirements directly

--- libraryBorrow/settings.py
+++ libraryBorrow/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage'
     'django.contrib.staticfiles',
+    'cloudinary'
     'django.contrib.sites',
     'lib',
     'widget_tweaks',
@@ -139,3 +141,11 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'

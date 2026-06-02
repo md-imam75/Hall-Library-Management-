916401aab3e90bb17c3d9ff53466450b8254cc42
Sabbir <imamsabbir20173@gmail.com>
2026-06-02 20:25:48 +0600

    Fix requirements typo for cloudinary storage

--- libraryBorrow/settings.py
+++ libraryBorrow/settings.py
@@ -25,9 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
     'django.contrib.sites',
     'lib',
     'widget_tweaks',
